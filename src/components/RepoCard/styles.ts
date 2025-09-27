bc0943e35d693538e0a3500440ee4e44ca65f3f7
Matheus Caetano <matheus.marques5215@gmail.com>
2025-09-27 16:07:27 -0300

    Continuação do projeto

--- src/components/RepoCard/styles.ts
+++ src/components/RepoCard/styles.ts
@@ -1,20 +1,96 @@
+import { Link } from 'react-router-dom';
 import styled, { css } from 'styled-components';
-import { RiBookmarkLine, RiStarLine } from 'react-icons/ri'
+import { RiBookMarkedLine, RiStarLine } from 'react-icons/ri'
 import { AiOutlineFork } from 'react-icons/ai'
 
 export const Container = styled.div`
-
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 16px;
+  border: 1px solid var(--border);
+  border-radius: 6px;
 `;
 
-export const Topside = styled.div``
+export const Topside = styled.div`
+  > header {
+    display: flex;
+    align-items: centers;
 
-export const Botside = styled.div``
+    > a {
+      margin-left: 8px;
+      font-size: 14px;
+      font-weight: 600;
+      color: var(--link);
 
-const iconCSS = css``
+      text-decoration: none;
 
-export const RepoIcon = styled(RiBookmarkLine)``
+      &:focus, &:hover {
+        text-decoration: underline;
+      }
+    }
+}
 
-export const StarIcon = styled(RiStarLine)``
+> p {
+  margin: 8px 0 16px;
+  font-size: 12px;
+  color: var(--gray);
+  letter-spacing: 0.1px;
+}
+`
 
-export const ForkIcon = styled(AiOutlineFork)``
+export const Botside = styled.div`
+  >ul {
+    display: flex;
+    align-items: center;
+  }
+
+  > li {
+    display: flex;
+    align-items: center;
+    margin-right: 16px;
+
+    > span {
+      margin-left: 5px;
+      font-size: 12px;
+      color: var(--gray);
+    }
+  }
+
+  .language {
+    width: 12px;
+    height: 12px;
+    border-radius: 50px;
+    flex-shrink: 0;
+
+    &.other {
+      background: var(--other-language)
+    }
+    &.javascript {
+      background: var(--javascript)
+    }
+    &.typescript {
+      background: var(--typescript)
+    }
+  }
+`
+
+const iconCSS = css`
+  width: 16px;
+  height: 16px;
+  fill: var(--icon);
+  flex-shrink: 0;
+`
+
+export const RepoIcon = styled(RiBookMarkedLine)`
+  ${iconCSS}
+`
+
+export const StarIcon = styled(RiStarLine)`
+  ${iconCSS}
+`
+
+export const ForkIcon = styled(AiOutlineFork)`
+  ${iconCSS}
+`
 
