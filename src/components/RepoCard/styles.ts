62e9d3602bed8bd8091edcb37401b7fd6e8b5b45
Matheus Caetano <matheus.marques5215@gmail.com>
2025-09-27 15:45:54 -0300

    Continuação do projeto

---
+++ src/components/RepoCard/styles.ts
@@ -0,0 +1,20 @@
+import styled, { css } from 'styled-components';
+import { RiBookmarkLine, RiStarLine } from 'react-icons/ri'
+import { AiOutlineFork } from 'react-icons/ai'
+
+export const Container = styled.div`
+
+`;
+
+export const Topside = styled.div``
+
+export const Botside = styled.div``
+
+const iconCSS = css``
+
+export const RepoIcon = styled(RiBookmarkLine)``
+
+export const StarIcon = styled(RiStarLine)``
+
+export const ForkIcon = styled(AiOutlineFork)``
+
--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
-import { Container, Main, LeftSide, RightSide } from './styles';
+import { Container, Main, LeftSide, RightSide, Repos } from './styles';
 
 import ProfileData from '../../components/ProfileData'
+import RepoCard from '../../components/RepoCard'
 
 
 const Profile: React.FC = () => {
@@ -23,7 +24,25 @@ const Profile: React.FC = () => {
           />
         </LeftSide>
 
-        <RightSide></RightSide>
+        <RightSide>
+          <Repos>
+            <h2>Random Repos</h2>
+
+            <div>
+              {[1, 2, 3, 4, 5, 6].map(n => (
+                <RepoCard
+                  key={n}
+                  username={'marqxmatheus'}
+                  reponame={'recriando-github'}
+                  description={'Recriando Github'}
+                  language={n % 3 === 0 ? 'JavaScript' : 'Typescript'}
+                  stars={8}
+                  forks={4}
+                />
+              ))}
+            </div>
+          </Repos>
+        </RightSide>
       </Main>
     </Container>
   );
