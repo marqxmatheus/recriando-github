75dbf472f19a3a6f19799a14e6c3f88883119f23
Matheus Caetano <matheus.marques5215@gmail.com>
2025-09-28 18:08:31 -0300

    Primeira versão 100% funcional

--- src/components/RepoCard/styles.ts
+++ src/components/RepoCard/styles.ts
@@ -1,7 +1,6 @@
-import { Link } from 'react-router-dom';
 import styled, { css } from 'styled-components';
-import { RiBookMarkedLine, RiStarLine } from 'react-icons/ri'
-import { AiOutlineFork } from 'react-icons/ai'
+import { RiBookMarkedLine, RiStarLine } from 'react-icons/ri';
+import { AiOutlineFork } from 'react-icons/ai';
 
 export const Container = styled.div`
   display: flex;
@@ -15,7 +14,7 @@ export const Container = styled.div`
 export const Topside = styled.div`
   > header {
     display: flex;
-    align-items: centers;
+    align-items: center;
 
     > a {
       margin-left: 8px;
@@ -25,72 +24,72 @@ export const Topside = styled.div`
 
       text-decoration: none;
 
-      &:focus, &:hover {
+      &:focus,
+      &:hover {
         text-decoration: underline;
       }
     }
-}
-
-> p {
-  margin: 8px 0 16px;
-  font-size: 12px;
-  color: var(--gray);
-  letter-spacing: 0.1px;
-}
-`
-
-export const Botside = styled.div`
-  >ul {
-    display: flex;
-    align-items: center;
   }
 
-  > li {
-    display: flex;
-    align-items: center;
-    margin-right: 16px;
-
-    > span {
-      margin-left: 5px;
-      font-size: 12px;
-      color: var(--gray);
-    }
+  > p {
+    margin: 8px 0 16px;
+    font-size: 12px;
+    color: var(--gray);
+    letter-spacing: 0.1px;
   }
-
-  .language {
-    width: 12px;
-    height: 12px;
-    border-radius: 50px;
-    flex-shrink: 0;
-
-    &.other {
-      background: var(--other-language)
-    }
-    &.javascript {
-      background: var(--javascript)
-    }
-    &.typescript {
-      background: var(--typescript)
-    }
-  }
-`
+`;
 
 const iconCSS = css`
   width: 16px;
   height: 16px;
   fill: var(--icon);
   flex-shrink: 0;
-`
+`;
 
 export const RepoIcon = styled(RiBookMarkedLine)`
   ${iconCSS}
-`
+`;
+
+export const Botside = styled.div`
+  > ul {
+    display: flex;
+    align-items: center;
+
+    > li {
+      display: flex;
+      align-items: center;
+      margin-right: 16px;
+
+      > span {
+        margin-left: 5px;
+        font-size: 12px;
+        color: var(--gray);
+      }
+    }
+  }
+
+  .language {
+    width: 12px;
+    height: 12px;
+    border-radius: 50%;
+    flex-shrink: 0;
+
+    &.other {
+      background: var(--other-language);
+    }
+    &.javascript {
+      background: var(--javascript);
+    }
+    &.typescript {
+      background: var(--typescript);
+    }
+  }
+`;
 
 export const StarIcon = styled(RiStarLine)`
   ${iconCSS}
-`
+`;
 
 export const ForkIcon = styled(AiOutlineFork)`
   ${iconCSS}
-`
-
+`;
