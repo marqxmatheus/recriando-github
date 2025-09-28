5a0a489e2bd15737f13ce3f4437f13e35a527495
Matheus Caetano <matheus.marques5215@gmail.com>
2025-09-28 17:27:49 -0300

    Continuação do projeto

--- src/pages/Repo/index.tsx
+++ src/pages/Repo/index.tsx
@@ -1,9 +1,45 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
-// import { Container } from './styles';
+import { Container, Breadcrumb, RepoIcon, Stats, StarIcon, ForkIcon, LinkButton, GithubIcon, } from './styles';
 
 const Repo: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <Breadcrumb>
+        <RepoIcon />
+        <Link className={'username'} to={'/marqxmatheus'}>
+          marqxmatheus
+        </Link>
+
+        <span></span>
+
+        <Link className={'reponame'} to={'/marqxmatheus/recriando-github'}>
+          recriando-github
+        </Link>
+      </Breadcrumb>
+
+      <p>Recriando a interface do Github utilizando React e Typescript</p>
+
+      <Stats>
+        <li>
+          <StarIcon />
+          <b>9</b>
+          <span>stars</span>
+        </li>
+        <li>
+          <ForkIcon />
+          <b>0</b>
+          <span>forks</span>
+        </li>
+      </Stats>
+
+      <LinkButton href={"https://github.com/marqxmatheus/recriando-github"}>
+        <GithubIcon />
+        <span>View on Github</span>
+      </LinkButton>
+    </Container>
+  )
 }
 
 export default Repo;
